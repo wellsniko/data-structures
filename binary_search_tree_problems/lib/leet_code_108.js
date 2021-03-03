339c896fe05b464b746fd63102240ddbce9f085a
Niko Wells <wellsniko@gmail.com>
2021-03-03 12:44:47 -0800

    binary search tree problems

--- binary_search_tree_problems/lib/leet_code_108.js
+++ binary_search_tree_problems/lib/leet_code_108.js
@@ -3,5 +3,16 @@
 
 
 function sortedArrayToBST(nums) {
-
+    var sortedArrayToBST = function(nums) {
+    if (!nums.length) return null
+    
+    let mid = Math.floor(nums.length/2)
+    
+    let root = new TreeNode(nums[mid])
+    
+    root.left = sortedArrayToBST(nums.slice(0, mid))
+    root.right = sortedArrayToBST(nums.slice(mid+1, nums.length))
+    
+    return root
+};
 }
--- binary_search_tree_problems/lib/leet_code_110.js
+++ binary_search_tree_problems/lib/leet_code_110.js
@@ -3,5 +3,20 @@
 
 
 function isBalanced(root) {
+    if(root === null) return true;
+    
+    var right = treeSize(root.right)
+    var left = treeSize(root.left)
+    var difference = Math.abs( right - left )
+    var result = true;
+    
+    if(difference > 1) result = false;
+    
+    return result && isBalanced(root.left) && isBalanced(root.right);
+};
 
-}
+var treeSize = function(root){
+    if(root === null) return 0;
+    return 1 + Math.max(treeSize(root.right), treeSize(root.left));
+
+};
